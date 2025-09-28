3ca8fa7796438d576174b2441d4e3d04472db58e
loiikoii <vappelquist03@gmail.com>
2025-09-28 14:07:58 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,14 +6,15 @@
         {
 
             {
+                //prints out welcome
                 Console.WriteLine("Välkommen till pyramidbyggaren!");
                 Console.Write("Hur hög ska den vara?: ");
                 int height = int.Parse(Console.ReadLine());
 
                 for (int i = 1; i <= height; i++)
                 {
-                    int block = 2 * i - 1;
-                    int space = height - i;
+                    int block = 2 * i - 1; //amount of blocks (*), 2x-1
+                    int space = height - i; //(Height)
 
                     Console.Write(new string(' ', space));
                     Console.WriteLine(new string('*', block));
